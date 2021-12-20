f137ce211a2f6e68a8bfb7c06e81d9f034adca6c
Geets19 <geets19@hotmail.com>
2021-12-20 22:57:53 +0000

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App"> Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Geetha Girithari and is
+        <a href="https://github.com/Geets19/react-weather-app" target="_blank">
+          {" "}
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
